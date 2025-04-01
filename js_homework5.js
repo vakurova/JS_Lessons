25b3e6b4aeec968a69e0d93a0c853b8bfab703ef
Hushevska Kateryna <vakurovakate@gmail.com>
2025-04-01 15:51:55 +0300

    Сума вартості послуг

--- js_homework5.js
+++ js_homework5.js
@@ -1,43 +1,49 @@
 var services = {
     "Стрижка": "60 грн",
     "Гоління": "80 грн",
-    "Миття голови": "100 грн"
+    "Миття голови": "100 грн",
+  
+    addService: function(name, price) {
+      this[name] = price + " грн";
+    },
+  
+    price: function () {
+      let sum = 0;
+      for (let key in this) {
+        if (typeof this[key] === "string" && this[key].includes("грн")) {
+          sum += parseFloat(this[key]);
+        }
+      }
+      return sum + " грн";
+    },
+  
+    minPrice: function () {
+      let prices = [];
+      for (let key in this) {
+        if (typeof this[key] === "string" && this[key].includes("грн")) {
+          prices.push(parseFloat(this[key]));
+        }
+      }
+      return Math.min(...prices) + " грн";
+    },
+  
+    maxPrice: function () {
+      let prices = [];
+      for (let key in this) {
+        if (typeof this[key] === "string" && this[key].includes("грн")) {
+          prices.push(parseFloat(this[key]));
+        }
+      }
+      return Math.max(...prices) + " грн";
+    }
   };
   
-  function addService(name, price) {
-    services[name] = price + " грн";
-  }
+  services.addService("Розбити скло", 200);
+  services.addService("Зачіска", 500);
+  services.addService("Масаж", 10);
   
-  function price(obj) {
-    let sum = 0; 
-    for (let key in obj) {
-      let value = parseFloat(obj[key]); 
-      sum += value; 
-    }
-    return sum + " грн";
-  }
+  console.log("Загальна вартість наданих послуг:", services.price());
+  console.log("Мінімальна ціна:", services.minPrice());
+  console.log("Максимальна ціна:", services.maxPrice());
   
-  function minPrice(obj) {
-    let prices = Object.values(obj).map(function(p) {
-      return parseFloat(p);
-    });
-    let min = Math.min(...prices);
-    return min + " грн";
-  }
-  
-  function maxPrice(obj) {
-    let prices = Object.values(obj).map(function(p) {
-      return parseFloat(p);
-    });
-    let max = Math.max(...prices);
-    return max + " грн";
-  }
-  
-  addService("Розбити скло", 200);
-  addService("Зачіска", 500);
-  addService("Масаж", 10);
- 
-  console.log("Загальна вартість наданих послуг:", price(services));
-  console.log("Мінімальна ціна:", minPrice(services));
-  console.log("Максимальна ціна:", maxPrice(services));
   
